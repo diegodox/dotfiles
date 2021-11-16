23113e9dcace831efc01b190262df2636bee50d1
Diego Fujii <android.mxdiego9@gmail.com>
2021-11-16 15:12:38 +0900

    fish update abbr

--- shell/.config/fish/config.fish
+++ shell/.config/fish/config.fish
@@ -1,18 +1,24 @@
 if status is-interactive
-    if command -v exa > /dev/null
-        alias ls='exa --header --icons --git'
+
+    # aliaces
+    if command -v exa >/dev/null
+        abbr -a ls 'exa --header --icons --git'
+        abbr -a ll 'exa --header --icons --git --long'
+        abbr -a la 'exa --header --icons --git --all'
+        abbr -a lla 'exa --header --icons --git --long --all'
+    else
+        abbr -a ll 'ls -l'
+        abbr -a la 'ls -a'
+        abbr -a lla 'ls -la'
     end
-    #abbr -a ll 'ls -l'
-    #abbr -a la 'ls -a'
-    #abbr -a lla 'ls -la'
-    if command -v bat > /dev/null
-        alias cat='bat'
+    if command -v ranger >/dev/null
+        abbr -a r ranger
     end
-    if command -v fd > /dev/null
-        alias find='fd'
+    if command -v lazygit >/dev/null
+        abbr -a lg lazygit
     end
-    if command -v nvim > /dev/null
-        alias vim='nvim'
+    if command -v bat >/dev/null
+        abbr -a cat bat
     end
 end
 
