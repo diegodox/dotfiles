4f109bbdfd20739d5c47743e69daae715d62e9d9
Diego Fujii <android.mxdiego9@gmail.com>
2021-12-24 21:53:37 +0900

    polybar use templature and i3layout module

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -9,5 +9,8 @@ while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 # Launch Polybar using default config location ~/.config/polybar/config
 polybar main &
 
+# Subscribe i3wm ipc to hook i3layout module update
+i3-msg -t subscribe -m '[ "workspace", "output", "window", "binding" ]' | while read line ; do polybar-msg hook i3layout 1 && sleep 0.2; done &
+
 echo "Polybar launched..."
 
