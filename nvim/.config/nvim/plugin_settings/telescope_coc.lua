cb2e5fb944050bf1607a681b5b06e8006fa4ffcd
Diego Fujii <android.mxdiego9@gmail.com>
2022-01-12 16:48:42 +0900

    nvim telescope coc keybinding

--- nvim/.config/nvim/plugin_settings/telescope_coc.lua
+++ nvim/.config/nvim/plugin_settings/telescope_coc.lua
@@ -10,12 +10,13 @@ wk.register(
             name = "coc",
             r = { "<Cmd>Telescope coc references<CR>", "references" },
             l = { "<Cmd>Telescope coc links<CR>", "links" },
+            i = { "<Cmd>Telescope coc implementations<CR>", "implementations" },
             c = { "<Cmd>Telescope coc commands<CR>", "coc commands" },
             t = { "<Cmd>Telescope coc type_definitions<CR>", "type definitions" },
             D = { "<Cmd>Telescope coc definitions<CR>", "definitions" },
             d = { "<Cmd>Telescope coc diagnostics<CR>", "diagnostics" },
             s = { "<Cmd>Telescope coc document_symbols<CR>", "document symbols" },
-            a = { "<Cmd>Telescope coc code_actions<CR>", "code actions" },
+            ["."] = { "<Cmd>Telescope coc code_actions<CR>", "code actions" },
         }
     },
     {
@@ -25,12 +26,12 @@ wk.register(
 wk.register(
     {
         name = "jump",
-        r = { "<Cmd>Telescope coc references<CR>", "references" },
-        l = { "<Cmd>Telescope coc links<CR>", "links" },
-        c = { "<Cmd>Telescope coc commands<CR>", "coc commands" },
-        t = { "<Cmd>Telescope coc type_definitions<CR>", "type definitions" },
-        d = { "<Cmd>Telescope coc definitions<CR>", "definitions" },
-        a = { "<Cmd>Telescope coc code_actions<CR>", "code actions" },
+        r = { "<Cmd>Telescope coc references<CR>", "CoC References" },
+        l = { "<Cmd>Telescope coc links<CR>", "CoC Links" },
+        i = { "<Cmd>Telescope coc implementations<CR>", "CoC Implementations" },
+        t = { "<Cmd>Telescope coc type_definitions<CR>", "CoC Type definitions" },
+        d = { "<Cmd>Telescope coc definitions<CR>", "CoC Definitions" },
+        ["."] = { "<Cmd>Telescope coc code_actions<CR>", "CoC Code actions" },
     },
     {
         prefix = "g"
