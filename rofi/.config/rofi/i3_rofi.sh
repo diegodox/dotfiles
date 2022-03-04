592f8eda9adeca38c5b977b670b0f43551d1498c
Diego Fujii <android.mxdiego9@gmail.com>
2022-03-04 15:35:18 +0900

    i3/rofi add rofi script to control i3wm

---
+++ rofi/.config/rofi/i3_rofi.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Rofi script to execute i3 commands
+
+command_list=(
+    "i3-msg -t command 'border toggle'"
+    "i3-msg -t command 'sticky toggle'"
+)
+command_label_list=(
+    "Toggle Border"
+    "Toggle Sticky (Floating Only)"
+)
+
+if ! command -v i3-msg &> /dev/null; then
+    # Print error if i3-msg not found
+    echo "Error: i3-msg could not be found."
+elif [[ -z "$*" ]]; then
+    # No arguments are passed, so display the menu.
+    for command_label in "${command_label_list[@]}"; do
+        echo "$command_label"
+    done
+else
+    # Some arguments passed, then run corresponding command.
+    for ((i = 0; i < ${#command_label_list[@]}; i++)); do
+        if [[ "$*" == "${command_label_list[$i]}" ]]; then
+            eval "${command_list[$i]} &> /dev/null"
+            break
+        fi
+        notify-send "Error: i3_rofi.sh" "No such command: $*"
+    done
+fi
