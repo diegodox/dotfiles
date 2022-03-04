083b2ea39073ebe22d8fd2858df16f824be84a55
Diego Fujii <android.mxdiego9@gmail.com>
2022-03-04 22:06:34 +0900

    fixup! rofi add workspace selection on i3_rofi.sh

--- rofi/.config/rofi/i3_rofi.sh
+++ rofi/.config/rofi/i3_rofi.sh
@@ -3,6 +3,8 @@
 # Rofi script to execute i3 commands
 # Internal status is kept on /tmp/rofi-i3-status
 
+status_file=/tmp/rofi-i3-status
+
 command_label_list=(
     "Toggle Border"
     "Toggle Sticky (Floating Only)"
@@ -19,15 +21,15 @@ initial_state() {
             i3-msg -t command 'sticky toggle' &> /dev/null
             ;;
         "Focus Workspace")
-            echo "Focus Workspace" > /tmp/rofi-i3-status
+            echo "Focus Workspace" > "$status_file"
             i3-msg -t get_workspaces | jq -r '.[] | "\(.name)"'
             ;;
         "Move window to Workspace")
-            echo "Move window to Workspace" > /tmp/rofi-i3-status
+            echo "Move window to Workspace" > "$status_file"
             i3-msg -t get_workspaces | jq -r '.[] | "\(.name)"'
             ;;
         *)
-            # echo "Internal Error: i3_rofi.sh" "No such command in initial_status: $*"
+            # echo "Internal Error: i3_rofi.sh" "No such command in initial_state: $*"
             ;;
     esac
 }
@@ -36,15 +38,15 @@ if ! command -v i3-msg &> /dev/null; then
     # Print error if i3-msg not found
     echo "Error: i3-msg could not be found."
 elif [[ -z "$*" ]]; then
-    # Initial state file
-    echo "" > /tmp/rofi-i3-status
+    # Initialize internal status file
+    echo "" > "$status_file"
     # No arguments are passed, so display the menu.
     for command_label in "${command_label_list[@]}"; do
         echo "$command_label"
     done
 else
-    # Some arguments passed, then find how to treat them by state.
-    case $(cat /tmp/rofi-i3-status) in
+    # Some arguments passed, then find how to treat them by internal state.
+    case $(cat "$status_file") in
         "Focus Workspace")
             i3-msg workspace "$*" &> /dev/null
             ;;
