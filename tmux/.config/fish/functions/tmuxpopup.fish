13d909abdcea086f64d6725f0fc21f6994205dab
diegodox <android.mxdiego9@gmail.com>
2023-03-01 17:43:46 +0900

    tmux: each session have own popup session

--- tmux/.config/fish/functions/tmuxpopup.fish
+++ tmux/.config/fish/functions/tmuxpopup.fish
@@ -1,11 +1,13 @@
 function tmuxpopup --description "toggle tmux popup window"
     set --function width '80%'
     set --function height '80%'
-    set --function session (tmux display-message -p -F "#{session_name}")
-    if contains popup $session
+    set --function current_session (tmux display-message -p -F "#{session_name}")
+    set --function main_session (echo $current_session | sed 's/^popup_//')
+    set --function popup_session "popup_"$main_session
+    if contains $current_session $popup_session
         tmux detach-client
     else
         tmux popup -d '#{pane_current_path}' -w$width -h$height \
-            -E "tmux attach -t popup || tmux new -s popup $argv"
+            -E "tmux attach -t $popup_session || tmux new -s $popup_session $argv"
     end
 end
